23bd50d8d374517f6568132f3ee3da0734207475
Le Hoang Vu <ThamTuMeoDen13@Les-MacBook-Pro.local>
2019-09-13 15:18:03 +0700

    commit source

--- src/view/App.js
+++ src/view/App.js
@@ -5,132 +5,136 @@ import Images from '../component/Images'
 import Buttons from '../component/Buttons'
 import WakeUp from '../component/WakeUp'
 import Footer from '../component/Footer'
+import Header from '../component/Header'
+import SignIn from '../component/SignIn'
 import { API_URL } from '../constant/config'
 import './App.css'
 
 const toastColor = {
-  background: '#505050',
-  text: '#fff'
+	background: '#505050',
+	text: '#fff'
 }
 
 export default class App extends Component {
 
-  state = {
-    loading: true,
-    uploading: false,
-    images: []
-  }
+	state = {
+		loading: true,
+		uploading: false,
+		images: []
+	}
 
-  componentDidMount() {
-    fetch(`${API_URL}/wake-up`)
-      .then(res => {
-        if (res.ok) {
-          return this.setState({ loading: false })
-        }
-        const msg = 'Something is went wrong with Heroku'
-        this.toast(msg, 'custom', 2000, toastColor)
-      })
-  }
+	componentDidMount() {
+		fetch(`${API_URL}/wake-up`)
+			.then(res => {
+				if (res.ok) {
+					return this.setState({ loading: false })
+				}
+				const msg = 'Something is went wrong with Heroku'
+				this.toast(msg, 'custom', 2000, toastColor)
+			})
+	}
 
-  toast = notify.createShowQueue()
+	toast = notify.createShowQueue()
 
-  onChange = e => {
-    const errs = []
-    const files = Array.from(e.target.files)
+	onChange = e => {
+		const errs = []
+		const files = Array.from(e.target.files)
 
-    // if (files.length > 3) {
-    //   const msg = 'Only 3 images can be uploaded at a time'
-    //   return this.toast(msg, 'custom', 2000, toastColor)
-    // }
+		// if (files.length > 3) {
+		//   const msg = 'Only 3 images can be uploaded at a time'
+		//   return this.toast(msg, 'custom', 2000, toastColor)
+		// }
 
-    const formData = new FormData()
-    const types = ['image/png', 'image/jpeg', 'image/gif']
+		const formData = new FormData()
+		const types = ['image/png', 'image/jpeg', 'image/gif']
 
-    files.forEach((file, i) => {
+		files.forEach((file, i) => {
 
-      if (types.every(type => file.type !== type)) {
-        errs.push(`'${file.type}' is not a supported format`)
-      }
+			if (types.every(type => file.type !== type)) {
+				errs.push(`'${file.type}' is not a supported format`)
+			}
 
-      // if (file.size > 150000) {
-      //   errs.push(`'${file.name}' is too large, please pick a smaller file`)
-      // }
+			// if (file.size > 150000) {
+			//   errs.push(`'${file.name}' is too large, please pick a smaller file`)
+			// }
 
-      formData.append(i, file)
-    })
+			formData.append(i, file)
+		})
 
-    if (errs.length) {
-      return errs.forEach(err => this.toast(err, 'custom', 2000, toastColor))
-    }
+		if (errs.length) {
+			return errs.forEach(err => this.toast(err, 'custom', 2000, toastColor))
+		}
 
-    this.setState({ uploading: true })
+		this.setState({ uploading: true })
 
-    console.log('image-upload', `${API_URL}/image-upload`)
-    fetch(`${API_URL}/image-upload`, {
-      method: 'POST',
-      body: formData
-    })
-      .then(res => {
-        if (!res.ok) {
-          throw res
-        }
-        return res.json()
-      })
-      .then(images => {
-        this.setState({
-          uploading: false,
-          images
-        })
-      })
-      .catch(err => {
-        err.json().then(e => {
-          this.toast(e.message, 'custom', 2000, toastColor)
-          this.setState({ uploading: false })
-        })
-      })
-  }
+		console.log('image-upload', `${API_URL}/image-upload`)
+		fetch(`${API_URL}/image-upload`, {
+			method: 'POST',
+			body: formData
+		})
+			.then(res => {
+				if (!res.ok) {
+					throw res
+				}
+				return res.json()
+			})
+			.then(images => {
+				this.setState({
+					uploading: false,
+					images
+				})
+			})
+			.catch(err => {
+				err.json().then(e => {
+					this.toast(e.message, 'custom', 2000, toastColor)
+					this.setState({ uploading: false })
+				})
+			})
+	}
 
-  filter = id => {
-    return this.state.images.filter(image => image.public_id !== id)
-  }
+	filter = id => {
+		return this.state.images.filter(image => image.public_id !== id)
+	}
 
-  removeImage = id => {
-    this.setState({ images: this.filter(id) })
-  }
+	removeImage = id => {
+		this.setState({ images: this.filter(id) })
+	}
 
-  onError = id => {
-    this.toast('Oops, something went wrong', 'custom', 2000, toastColor)
-    this.setState({ images: this.filter(id) })
-  }
+	onError = id => {
+		this.toast('Oops, something went wrong', 'custom', 2000, toastColor)
+		this.setState({ images: this.filter(id) })
+	}
 
-  render() {
-    const { loading, uploading, images } = this.state
+	render() {
+		const { loading, uploading, images } = this.state
 
-    const content = () => {
-      switch (true) {
-        case loading:
-          return <WakeUp />
-        case uploading:
-          return <Spinner />
-        case images.length > 0:
-          return <Images
-            images={images}
-            removeImage={this.removeImage}
-            onError={this.onError}
-          />
-        default:
-          return <Buttons onChange={this.onChange} />
-      }
-    }
+		const content = () => {
+			switch (true) {
+				case loading:
+					return <WakeUp />
+				case uploading:
+					return <Spinner />
+				case images.length > 0:
+					return <Images
+						images={images}
+						removeImage={this.removeImage}
+						onError={this.onError}
+					/>
+				default:
+					return <Buttons onChange={this.onChange} />
+			}
+		}
 
-    return (
-      <div className='container'>
-        <Notifications />
-        <div className='buttons'>
-          {content()}
-        </div>
-        <Footer />
-      </div>
-    )
-  }
+		return (
+			<div className='container'>
+				<Notifications />
+				<Header />
+				<div className='buttons'>
+					{content()}
+				</div>
+				<Footer />
+				{/* <SignIn /> */}
+			</div>
+		)
+	}
 }
