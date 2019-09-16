33cff8778ff13cba5b4eb59e0a659c9546f86863
Le Hoang Vu <ThamTuMeoDen13@Les-MacBook-Pro.local>
2019-09-16 11:25:10 +0700

    save prevou souce example

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,3 +1,5 @@
 export const API_URL = process.env.NODE_ENV === 'production'
   ? 'https://react-image-upload.herokuapp.com'
   : 'http://localhost:8080'
+
+export const APOLLO_API_URL = 'http://localhost:8383'
--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './view/App'
+// import App1 from './view/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+import { APOLLO_API_URL } from './constant/config'
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: APOLLO_API_URL,
+});
+
+const defaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'cache-and-network',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'all',
+    },
+    mutate: {
+        errorPolicy: 'all',
+    },
+};
+const client = new ApolloClient({
+    // Provide required constructor fields
+    cache: cache,
+    link: link,
+
+    // Provide some optional constructor fields
+    name: 'react-web-client',
+    version: '1.3',
+    queryDeduplication: false,
+    defaultOptions: defaultOptions,
+});
+
+const AppRoot = () => (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
+ReactDOM.render(<AppRoot />, document.getElementById('root'))
 registerServiceWorker()
