8ee6c957561ca2255cd04670a4030e19f387943d
Le Hoang Vu <ThamTuMeoDen13@Les-MacBook-Pro.local>
2019-10-30 14:30:32 +0700

    Updates

---
+++ precache-manifest.4029a1cc0f9b8af1ba89f562e5bb5d8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddd155b8ad2c413baff744fe570c66d",
+    "url": "/sunday-tv-client/index.html"
+  },
+  {
+    "revision": "ed32250564084161831f",
+    "url": "/sunday-tv-client/static/css/2.2fe65d49.chunk.css"
+  },
+  {
+    "revision": "ed32250564084161831f",
+    "url": "/sunday-tv-client/static/js/2.cf848905.chunk.js"
+  },
+  {
+    "revision": "40a9622c0112124b0eaf",
+    "url": "/sunday-tv-client/static/js/main.fcaef64d.chunk.js"
+  },
+  {
+    "revision": "565538ae40b0c151621d",
+    "url": "/sunday-tv-client/static/js/runtime~main.800eb149.js"
+  }
+]);
